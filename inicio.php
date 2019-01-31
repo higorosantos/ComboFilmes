8df14b8dcfbbf7af4a9077d3cc09235da6e9092a
kimbres <lucas.casxr8@gmail.com>
2019-01-31 19:52:14 -0200

    footer

---
+++ inicio.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <title>Combo filmes</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+<!-- Footer Links -->
+<div class="container text-center text-md-left mt-5">
+
+<!-- Grid row -->
+<div class="row mt-3 dark-grey-text">
+
+  <!-- Grid column -->
+  <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
+
+    <!-- Content -->
+    <h6 class="text-uppercase font-weight-bold">Mystery Box</h6>
+    <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+    <p>Site de Vendas de produtos Nerds, assinatura Mensal de caixas com itens surpresa e artigos sobre temas.</p>
+
+  </div>
+  <!-- Grid column -->
+
+  <!-- Grid column -->
+  <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+    <!-- Links -->
+    <h6 class="text-uppercase font-weight-bold">Produtos</h6>
+    <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+    <p>
+      <a class="dark-grey-text" href="#!">Box Assinatura</a>
+    </p>
+    <p>
+      <a class="dark-grey-text" href="#!"> Box Avulsa</a>
+    </p>
+    <p>
+      <a class="dark-grey-text" href="#!">Camisas</a>
+    </p>
+    <p>
+      <a class="dark-grey-text" href="#!">Temas</a>
+    </p>
+
+  </div>
+  <!-- Grid column -->
+
+  <!-- Grid column -->
+  <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+    <!-- Links -->
+    <h6 class="text-uppercase font-weight-bold">Links úteis</h6>
+    <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+    <p>
+      <a class="dark-grey-text" href="#!">Sua Conta</a>
+    </p>
+
+    <p>
+      <a class="dark-grey-text" href="#!">Taxas de envio</a>
+    </p>
+    <p>
+      <a class="dark-grey-text" href="#!">Ajuda</a>
+    </p>
+
+  </div>
+  <!-- Grid column -->
+
+  <!-- Grid column -->
+  <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+    <!-- Links -->
+    <h6 class="text-uppercase font-weight-bold">Contato</h6>
+    <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+    <p>
+      <i class="fas fa-home mr-3"></i> Brasil, RJ 23085-000, BR</p>
+    <p>
+      <i class="fas fa-envelope mr-3"></i> suporte@mysterybox.com</p>
+    <p>
+      <i class="fas fa-phone mr-3"></i> + 55 21 99206-7444</p>
+
+
+  </div>
+  <!-- Grid column -->
+
+</div>
+<!-- Grid row -->
+
+</div>
+<!-- Footer Links -->
+
+<!-- Copyright -->
+<div class="footer-copyright text-center text-black-50 py-3">© 2019 Copyright:
+<a class="dark-grey-text" href="https://mdbootstrap.com/education/bootstrap/"> Mysterybox</a>
+</div>
+<!-- Copyright -->
+
+</footer>
+    
+</body>
+</html>
